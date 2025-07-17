d9dc4eeb6766af83596aa9eb51e0f416b238abf6
azoozin <155941153+azoozin@users.noreply.github.com>
2025-07-17 06:39:44 -0300

    1.3 exercise refactor App

--- part-1/course_information/src/App.jsx
+++ part-1/course_information/src/App.jsx
@@ -4,18 +4,27 @@ import Total from './components/Total';
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+
+  }
 
   const parts = [
-    {name: part1, exercises: exercises1},
-    {name: part2, exercises: exercises2},
-    {name: part3, exercises: exercises3},
+    {name: part1.name, exercises: part1.exercises},
+    {name: part2.name, exercises: part2.exercises},
+    {name: part3.name, exercises: part3.exercises},
   ]
+
 
   return (
     <div>
