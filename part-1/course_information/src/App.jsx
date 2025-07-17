4297f680709d42656a039d15cd377cfa578be222
azoozin <155941153+azoozin@users.noreply.github.com>
2025-07-17 06:42:04 -0300

    ex 1.4

--- part-1/course_information/src/App.jsx
+++ part-1/course_information/src/App.jsx
@@ -5,26 +5,20 @@ import Total from './components/Total';
 const App = () => {
   const course = 'Half Stack application development'
 
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-
-  }
-
-  const parts = [
-    {name: part1.name, exercises: part1.exercises},
-    {name: part2.name, exercises: part2.exercises},
-    {name: part3.name, exercises: part3.exercises},
+    const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
   ]
-
 
   return (
     <div>
