e2f32a302bda3ebef8e154cb49b2c2cad8b7b4b9
pacarana <155941153+azoozin@users.noreply.github.com>
2025-07-19 18:14:25 -0300

    anecdotes ex 1.12

--- part-1/anecdotes/src/App.jsx
+++ part-1/anecdotes/src/App.jsx
@@ -16,7 +16,8 @@ const App = () => {
 
   return (
     <div>
-      {anecdotes[selected]}
+      <p>{anecdotes[selected]}</p>
+      <button onClick={() => {setSelected(Math.floor(Math.random() * 8))}}>next anecdote</button>
     </div>
   )
 }
