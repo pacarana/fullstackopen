f9df2923efc8e1c73430a73d5ff9611dc0d033b6
pacarana <155941153+azoozin@users.noreply.github.com>
2025-07-21 06:38:56 -0300

    anecdotes ex 1.14

--- part-1/anecdotes/src/App.jsx
+++ part-1/anecdotes/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const App = () => {
   const anecdotes = [
@@ -14,6 +14,10 @@ const App = () => {
    
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(new Uint8Array(8))
+  const [mostVoted, setMostVoted] = useState({
+    anecdote: null,
+    votes: 0
+  });
 
   const handleVote = () => {
     const newVotes = [...votes]
@@ -21,12 +25,41 @@ const App = () => {
     setVotes(newVotes)
   }
 
+  const handleMostVoted = () => {
+    let mostVotedObj = {
+      anecdote: anecdotes[0],
+      votes: votes[0]
+    }
+
+    for (let i = 0; i < votes.length; i++) {
+      if (votes[i] >= mostVotedObj.votes) {
+        mostVotedObj.anecdote = anecdotes[i];
+        mostVotedObj.votes = votes[i];
+      }
+    }
+
+    setMostVoted(mostVotedObj);
+  }
+
+  useEffect(() => {
+    handleMostVoted();
+  }, [votes]);
+
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
-      <button onClick={handleVote}>vote</button>
-      <button onClick={() => {setSelected(Math.floor(Math.random() * 8))}}>next anecdote</button>
+      <section>
+        <h1>Anecdote of the day</h1>
+        <p>{anecdotes[selected]}</p>
+        <p>has {votes[selected]} votes</p>
+        <button onClick={handleVote}>vote</button>
+        <button onClick={() => {setSelected(Math.floor(Math.random() * 8))}}>next anecdote</button>
+      </section>
+        
+      <section>
+        <h1>Anecdote with most votes</h1>
+        <p>{mostVoted.anecdote}</p>
+        <p>had {mostVoted.votes} votes</p>
+      </section>
     </div>
   )
 }
