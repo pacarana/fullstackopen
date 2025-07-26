69987d3e2ae47acea7c8698947d86d2f72ab7bd6
pacarana <155941153+azoozin@users.noreply.github.com>
2025-07-26 16:43:03 -0300

    ex 2.1

---
+++ part-2/courseinformation/src/components/Content.jsx
@@ -0,0 +1,17 @@
+import Part from './Part'
+
+const Content = ({parts}) => {
+    return (
+        <>
+            {/* 
+            NOTE:
+                Can change '{}' for '()' in map function to implicitly return instead of using return command 
+            */}
+            {parts.map((part, key) => {
+               return <Part name={part.name} exercises={part.exercises} />
+            })}
+        </>
+    );
+}
+
+export default Content;
